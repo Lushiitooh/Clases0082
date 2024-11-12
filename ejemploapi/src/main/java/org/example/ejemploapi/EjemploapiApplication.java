dafe8fac58179e58f0b30ed8001ffe599f9597f7
Lushiitooh <luis.sepu.c@gmail.com>
2024-11-12 19:31:22 -0300

    Ejercicio LocalApi

---
+++ ejemploapi/src/main/java/org/example/ejemploapi/EjemploapiApplication.java
@@ -0,0 +1,15 @@
+package org.example.ejemploapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EjemploapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjemploapiApplication.class, args);
+    }
+
+}
---
+++ ejemploapi/src/test/java/org/example/ejemploapi/EjemploapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ejemploapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
